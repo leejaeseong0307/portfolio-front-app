b3336f799cb321bbe1301a631bb16221f94957be
leejaeseong0307 <leejaeseong0307@gmail.com>
2025-06-14 16:16:13 +0900

    푸터 스크롤에 따른 show,hide기능 추가

--- login-app-frontend/src/components/Footer.js
+++ login-app-frontend/src/components/Footer.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../css/Footer.css';
 
 const Footer = () => {
+  const [visible, setVisible] = useState(true);
+  let lastScrollY = window.scrollY;
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY < lastScrollY) {
+        setVisible(true); // 스크롤 위로
+      } else {
+        setVisible(false); // 스크롤 아래로
+      }
+      lastScrollY = window.scrollY;
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <footer className="footer-fixed text-center">
-      <p>© 2025 Oolrim. All rights reserved.</p>
+    <footer className={`footer-fixed text-center ${visible ? '' : 'hidden'}`}>
+      <p>© 2025 Ool-Rrim. All rights reserved.</p>
       <p>Contact: leejaeseong0307@gmail.com</p>
       <p><a href="/terms" className="footer-link">이용약관</a> | <a href="/privacy" className="footer-link">개인정보처리방침</a></p>
     </footer>
